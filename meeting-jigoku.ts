32efe8793b455baa80bc45017e7987f2fc7d77f3
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2025-06-04 17:04:24 +1200

    add score calculation function

--- meeting-jigoku.ts
+++ meeting-jigoku.ts
@@ -8,18 +8,14 @@ function planMyDay(invitations: Array<Invitation>): Array<Invitation> {
     return b.duration - a.duration
   }
   const sorted = invitations.toSorted(sortByDescending)
-  console.log(sorted)
   function conflictsWithAny(invitation: Invitation) {
     // console.log(invalidTimeslots.length + 'length')
-    console.log(sorted.length)
     for (let i = 0; i < sorted.length; i++) {
-      console.log("I'm trying!")
       const start = invitation.start
       const end = invitation.start + invitation.duration
       const confirmingStart = sorted[i].start
       const confirmingEnd = sorted[i].start + sorted[i].duration
       if (
-        length < 1 ||
         (start >= confirmingStart && start <= confirmingEnd) ||
         (end >= confirmingStart && end <= confirmingEnd)
       ) {
@@ -36,12 +32,17 @@ function planMyDay(invitations: Array<Invitation>): Array<Invitation> {
       confirmed.push(unconfirmed)
     }
   }
-
   function byStartAscending(a: Invitation, b: Invitation) {
-    return a.start, b.start
+    return a.start - b.start
   }
   const schedule = confirmed.toSorted(byStartAscending)
+
+  var score = schedule.reduce(function (prev, cur) {
+    return prev + cur.duration
+  }, 0)
+  console.log(score)
+  console.log(schedule)
   return schedule
 }
 
-console.log(planMyDay(invitations))
+planMyDay(invitations)
